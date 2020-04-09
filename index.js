cc3d06c1c1d34ff6683da92a8010274f21a8c4a3
jkuenzinger <jkuenzinger228@gmail.com>
2020-04-09 10:22:43 -0700

    fixed syntax issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./api/server.js");
 
 // make the port dynamic so heroku can assign one
 
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
